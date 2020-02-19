f177de46c974d12c128c2c81cc61984826a46553
chenpan01 <49231737+chenpan01@users.noreply.github.com>
2020-02-19 20:19:57 +0800

    定位程序中算法函数

--- AnalysisAlgorithmFunc.py
+++ AnalysisAlgorithmFunc.py
@@ -1,4 +1,5 @@
 # -*- coding:utf-8 -*-
+第一种方法：
 def mul():
     return 1
 
@@ -70,6 +71,49 @@ def GetKeyFunc(Start, End):
 			#如果算术或逻辑指令的个数大于1，则可以初步判断该函数为用户写的算法函数(有误差)
 			if OpNum>2:
 				FindFunc[FunctionName]=FunAddress[i]
+				#print "i:",FunAddress[i],"i+1:",FunAddress[i+1]
+				
+	for Name, ea in FindFunc.items():
+		print Name, ":", ea
+for seg in Segments():  # 遍历所有的段
+    if SegName(seg) == '.text':
+        GetKeyFunc(seg, SegEnd(seg))
+
+第二种方法：
+OpAndTypeNum=dict()
+FindFunc = dict()
+def IsDealFu(Op):
+	if Op=='mul' or Op=='imul' or Op=='and' or Op=='or' or Op=='not' or Op=='div' or Op=='xor':
+		return 1
+	else:
+		return 0
+def GetKeyFunc(Start, End):
+	
+	for function_ea in Functions(Start, End):
+		FunctionName = GetFunctionName(function_ea)
+		#获取下个函数地址
+		NextFunctionAddr=NextFunction(function_ea)
+		#判断是否是用户名、地址是否合法
+		if NextFunctionAddr!= End and FunctionName[0:3]=='sub' and len(str(NextFunctionAddr))<9:
+			OpNum=0
+			OpAndTypeNum.clear()
+			
+			for singfuc_ea in range(function_ea,NextFunctionAddr):
+				flag = GetFlags(singfuc_ea)
+				
+				if isCode(flag):
+					
+					op = GetMnem(singfuc_ea)
+					#统计指令数量
+					if IsDealFu(op)==1:
+						OpAndTypeNum[op] = OpAndTypeNum.get(op,0)+1
+			#统计算术或逻辑指令的个数
+			for OP,value in OpAndTypeNum.items():
+				if value>0:
+					OpNum+=1					
+			#如果算术或逻辑指令的个数大于2，则可以初步判断该函数为用户写的算法函数(有误差)
+			if OpNum>2:
+				FindFunc[FunctionName]=function_ea
 				#print "i:",FunAddress[i],"i+1:",FunAddress[i+1]
 				
 	for Name, ea in FindFunc.items():
